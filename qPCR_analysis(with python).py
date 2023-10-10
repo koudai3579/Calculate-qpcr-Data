1060124a56729f1be74ff68fbb0d08b62c0b9a58
Okamura-Koudai <koudaisuper@icloud.com>
2023-10-10 20:52:23 +0900

    2023/10/10

--- qPCR_analysis(with python).py
+++ qPCR_analysis(with python).py
@@ -1,4 +1,9 @@
+#memo
+#実行：右上の▷
+
 import errno
+import subprocess
+from datetime import datetime
 from openpyxl.utils import get_column_letter
 import openpyxl as excel
 from openpyxl.styles import Alignment
@@ -9,6 +14,10 @@ from openpyxl.drawing.fill import PatternFillProperties, ColorChoice
 from openpyxl.chart.marker import DataPoint
 from openpyxl.chart.label import DataLabel, DataLabelList
 from openpyxl.styles.borders import Border, Side
+from openpyxl.chart import LineChart, Reference, Series
+from openpyxl.chart.error_bar import ErrorBars
+import xlwings as xw
+
 
 #シート読み込み
 sheet = excel.load_workbook("qpcr-data.xlsx", data_only=True).active
@@ -17,16 +26,17 @@ book = excel.Workbook()
 resultSheet = book.active
 
 data1 = [] #アクチン
-data2 = [] #遺伝子1
+data2 = [] #評価遺伝子
 
+#各列のデータを取得
 for i in range(1,7):
-    #アクチンの行
+    #アクチンの列
     if i == 1:
         for j in range(3,27):
             value = sheet.cell(row=j,column=i).value
             if value != None:
                 data1.append(value)
-   #遺伝子1の行            
+   #評価遺伝子の列           
     if i == 2:
         for j in range(3,27):
             value = sheet.cell(row=j,column=i).value
@@ -38,9 +48,10 @@ length1 = len(data1)
 length2 = len(data2)
 
 #入力してもらったサンプル数（N）を取得
-n = sheet.cell(row=2,column=9).value
+n = sheet.cell(row=2,column=4).value
 number_of_data = len(data1)/n
-#遺伝子名を配列で取得
+
+#遺伝子名を配列で取得(アクチンと評価遺伝子の2つ)
 genes = []
 for i in range(1,3):
     gene = sheet.cell(row=2,column=i).value
@@ -51,12 +62,12 @@ for i in range(1,3):
 resultSheet.append (["計算("+genes[1]+")",genes[0],genes[1],"power","avepower","相対値PR1","相対値平均PR1","S.E.（誤差）"])
 
 #各データの書き込み
-# data1(act)
+# data1(アクチン)
 a = 2
 for i ,data in enumerate(data1):
     resultSheet["B" + str(a)] = data
     a += 1
-# data2(gene1)
+# data2(評価遺伝子)
 a = 2
 for i ,data in enumerate(data2):
     resultSheet["C" + str(a)] = data
@@ -64,12 +75,13 @@ for i ,data in enumerate(data2):
 
 #サンプルネームを取得し設定
 sampleNames = []
-for i in range(9,14):
+for i in range(5,9):
     sampleName = sheet.cell(row=3,column=i).value
     if sampleName == None:
         sampleName = "未入力"
     sampleNames.append(sampleName)
 
+#サンプル名の書き込みおよびN数に応じたセルの結合
 a=2
 for number , sampleName in enumerate(sampleNames): 
     if a > length1:
@@ -80,36 +92,26 @@ for number , sampleName in enumerate(sampleNames):
         resultSheet["A"+str(a)].alignment = Alignment(horizontal="center", vertical="center")
     a += n
 
-resultSheet["J2"] = "結果1（相対値平均pr1）"
+#計算結果用基礎を書き込み→グラフ作成に利用
+resultSheet["K2"] = "相対値平均pr1"
 resultSheet["J3"] = sampleNames[0]
 resultSheet["J4"] = sampleNames[1]
 resultSheet["J5"] = sampleNames[2]
 resultSheet["J6"] = sampleNames[3]
-resultSheet["J7"] = sampleNames[4]
-resultSheet["K2"] = genes[1]
-resultSheet["L2"] = "結果2（誤差）"
-resultSheet["L3"] = sampleNames[0]
-resultSheet["L4"] = sampleNames[1]
-resultSheet["L5"] = sampleNames[2]
-resultSheet["L6"] = sampleNames[3]
-resultSheet["L7"] = sampleNames[4]
-resultSheet["R2"] = genes[1]
-
+resultSheet["J2"] = genes[1]
+resultSheet["L2"] = "誤差"
 
 #power値を算出
-#gene1
 for i in range(length2):
     target = resultSheet.cell(row=i+2,column=3).value
     housekeeping = resultSheet.cell(row=i+2,column=2).value
 
-    if target == "N/A" or housekeeping == "N/A" :
-        continue
+    if target == "N/A" or housekeeping == "N/A" :continue
 
     power = 2 ** (-(target- housekeeping))
     resultSheet.cell(row= (i+2), column=4,value=power)
 
-#avepower
-# data2(gene1)
+#avepowerを算出
 a=2
 for number , sample in enumerate(data2): 
     if a > length2:
@@ -130,21 +132,19 @@ for number , sample in enumerate(data2):
     a += n
 
 #相対値PR1値を算出
-# data2(gene1)
 for i in range(length2):
     # power取得
     power = resultSheet.cell(row= (i+2), column=4).value
     if power == None:
         continue
     else:
-        # 各pr1と比較するbase_avepower取得
+        #各pr1と比較するbase_avepower取得
         base_avepower = resultSheet.cell(row= 2, column=5).value
         #計算結果を出力
         pr1 = power/base_avepower
         resultSheet.cell(row=i+2,column=6,value = pr1)
 
 #相対値平均PR1算出
-#data2(gene1)
 c=2
 for number , sample in enumerate(data2): 
     if c > length2:
@@ -163,16 +163,16 @@ for number , sample in enumerate(data2):
             avepr1 = sum/copy_n
             resultSheet.cell(row=c,column=7,value = avepr1)
     c += n
-#求めた相対値平均PR1を要約スペースにも書き込み
+
+#求めた相対値平均PR1を要約スペース(グラフ作成用)にも書き込み
 e = 2
 for i in range(n-1):
     avepr1 = resultSheet. cell(row=e,column=7).value
     resultSheet.cell(row=i+3,column=11,value=avepr1)
     e += n
 
-#誤算算出 data2はまだ値が入っている前提でしか作れていなかった？？？
-#data2
-a=2
+#誤算算出
+a = 2
 for number , sample in enumerate(data2): 
     if a > length2:
         break
@@ -193,25 +193,37 @@ for number , sample in enumerate(data2):
         resultSheet.cell(row=a,column=8,value = gosa)
     a += n
 
-#グラフ表示
+#求めた誤差を要約スペース(グラフ作成用)にも書き込み
+e = 2
+for i in range(n-1):
+    gosa = resultSheet.cell(row=e,column=8).value
+    resultSheet.cell(row=i+3,column=12,value=gosa)
+    e += n
+
+# グラフ表示
 chart = BarChart()
 chart.width = 20
 chart.height = 14
 chart.title = "Result"              
 chart.y_axis.title = '発現量'
 chart.legend.position = 'b' 
-chart_data = Reference(resultSheet,min_col=11, max_col= 9+len(genes) , min_row=2, max_row= 2+number_of_data)
-chart_category= Reference(resultSheet,min_col=10, min_row=3, max_row= 2+number_of_data)
+chart_data = Reference(resultSheet, min_col=11, max_col=11, min_row=2, max_row=2 + number_of_data)
+chart_category = Reference(resultSheet, min_col=10, min_row=3, max_row=2 + number_of_data)
 chart.add_data(chart_data, titles_from_data=True)
 chart.set_categories(chart_category) 
 chart.style = 7
 chart.type = "col"
 chart.grouping = "standard"
 chart.gapwidth = 15
-resultSheet.add_chart(chart,"J10")
+
+# エラーバーを追加してグラフを完成　なぜかエラーバーが表示されていない
+error_bars_data = Reference(resultSheet, min_col=12, max_col=12, min_row=3, max_row=2 + number_of_data)
+error_bars = ErrorBars()
+error_bars.errorBarType = "custom"
+chart.series[0].errorBars = error_bars
+resultSheet.add_chart(chart, "J10")
 
 # 罫線設定(行目よりn数ごとに分割)
-# gene1
 border = Border(bottom=Side(style='thin', color='000000'))
 for number , sample in enumerate(data1):
     if (number + 1)  %  n  == 0:
@@ -230,8 +242,11 @@ for col in resultSheet.columns:
     adjusted_width = (max_length + 2) * 1.2
     resultSheet.column_dimensions[get_column_letter(column)].width = adjusted_width
 
-#ファイルの保存
-book.save("result_qpcr.xlsx")
-#ファイルオープン
+#ファイル名を付け、シートをローカルに保存
+current_datetime = datetime.now()
+formatted_datetime = current_datetime.strftime("%Y:%m:%-d:%H:%M")
+book.save(f"result_qpcr_{formatted_datetime}.xlsx")
 
-#誤差の要約スペース　グラフへの反映
+#保存したファイルを自動でオープン(iosのみ有効らしい)
+excel_file_path = f"result_qpcr_{formatted_datetime}.xlsx"
+subprocess.Popen(['open', excel_file_path])
